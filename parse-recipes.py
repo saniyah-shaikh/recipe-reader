2274971e5f7a3dcdd9ef2d9e56f35daec8b2e83a
saniyah-shaikh <saniyah@seas.upenn.edu>
2018-01-20 13:35:06 -0500

    basic outline

---
+++ parse-recipes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 20 13:26:01 2018
+
+@author: Saniyah
+"""
+
+
+import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def parse_recipe():
+    return 0
+
+def parse_page_of_recipe_links():
+    return 0
+
+def parse_all_recipes():
+    return 0
